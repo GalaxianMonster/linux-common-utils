4f9542b7f367316e6fa0f6fe3a6a08d368010281
GalaxianMonster <dtbernal13@gmail.com>
2021-06-06 20:24:17 +0800

    improved listdir

--- listdir.c
+++ listdir.c
@@ -38,6 +38,16 @@ int pchar_cmp_same(char* str1, char* str2)
 	/* If it reaches this code then return 1 which is true */
 	return 1;
 }
+char* get_type(char* path)
+{
+	struct stat new_stat;
+	stat(path, &new_stat);
+	if(S_ISDIR(new_stat.st_mode))
+		return "Directory";
+	else
+		return "File";
+}
+
 
 int main(int argc, char **argv)
 {
@@ -55,12 +65,16 @@ int main(int argc, char **argv)
         printf("\033[31mError:\033[0m Directory(In Argument 2) does not exists or is not a file\n");
         exit(-1);
     }
-    printf("--[ Directory \"%s\" Childs ]--\n", realpath(argv[1], NULL));
+    printf("-- [ Directory \"%s\" ] --\n", realpath(argv[1], NULL));
     while((ent = readdir(dir)) != NULL)
     {
 	if(pchar_cmp_same(ent->d_name, ".") == 0)
 		if(pchar_cmp_same(ent->d_name, "..") == 0)
-			printf("%s\n", ent->d_name);
+		{
+			char* path_r = (char*)malloc((strlen(ent->d_name) + strlen(realpath(argv[1], NULL)) + 2) * sizeof(char));
+			sprintf(path_r, "%s/%s", realpath(argv[1], NULL), ent->d_name);
+			printf("%s: %s\n", get_type(path_r), ent->d_name);
+		}
     }
     closedir(dir);
     free(ent);
